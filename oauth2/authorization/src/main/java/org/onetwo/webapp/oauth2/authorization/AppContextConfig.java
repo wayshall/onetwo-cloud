07d1e83f3b6ea141d0f3d8e763bfeb072b3f8577
wayshall <weishao.zeng@gmail.com>
2016-12-22 17:42:46 +0800

    edit pom

--- oauth2/authorization/src/main/java/org/onetwo/webapp/oauth2/authorization/AppContextConfig.java
+++ oauth2/authorization/src/main/java/org/onetwo/webapp/oauth2/authorization/AppContextConfig.java
@@ -1,19 +1,13 @@
 package org.onetwo.webapp.oauth2.authorization;
 
-import org.onetwo.boot.module.security.url.EnableJFishUrlSecurity;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.onetwo.boot.module.security.url.EnableUrlSecurity;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerEndpointsConfiguration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpointHandlerMapping;
 
 @Configuration
 //@EnableOAuth2Client
 @EnableAuthorizationServer
-@EnableJFishUrlSecurity
+@EnableUrlSecurity
 public class AppContextConfig{
 	
 	/*@Configuration
