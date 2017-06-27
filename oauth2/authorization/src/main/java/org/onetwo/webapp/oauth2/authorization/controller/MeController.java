01eb3e1a9fd8b7717f8bde423d5b6baea7311516
wayshall <weishao.zeng@gmail.com>
2017-06-27 17:59:09 +0800

    upgrade onetwo

--- oauth2/authorization/src/main/java/org/onetwo/webapp/oauth2/authorization/controller/MeController.java
+++ oauth2/authorization/src/main/java/org/onetwo/webapp/oauth2/authorization/controller/MeController.java
@@ -2,7 +2,7 @@ package org.onetwo.webapp.oauth2.authorization.controller;
 
 import org.onetwo.common.data.AbstractDataResult.SimpleDataResult;
 import org.onetwo.common.date.DateUtils;
-import org.onetwo.common.spring.mvc.utils.WebResultCreator;
+import org.onetwo.common.spring.web.mvc.utils.WebResultCreator;
 import org.onetwo.webapp.oauth2.authorization.vo.UserProfileVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
